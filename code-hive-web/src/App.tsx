f67fd3300d56ca75574c53f05962640edb49f2f8
Maicon Vitor Theodoro da Silva <maicon.vitorsilva@hotmail.com>
2026-03-27 03:05:11 -0300

    feat: integrar CosmosBackground e configurar rotas principais

--- code-hive-web/src/App.tsx
+++ code-hive-web/src/App.tsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import VagasDev from './pages/VagasDev';
-import VagasAdv from './pages/VagasAdv';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import VagasDev from "./pages/VagasDev";
+import VagasAdv from "./pages/VagasAdv";
+import CosmosBackground from "./components/CosmosBackground";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vagas-dev" element={<VagasDev />} />
-        <Route path="/vagas-adv" element={<VagasAdv />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <CosmosBackground />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/vagas-dev" element={<VagasDev />} />
+          <Route path="/vagas-adv" element={<VagasAdv />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
