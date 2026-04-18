a7058d2a2eba5f2d4086f58e0ee103680bf96c8a
Maicon Vitor Theodoro da Silva <maicon.vitorsilva@hotmail.com>
2026-04-18 20:16:03 -0300

    refactor: integra novos componentes e rotas no App

--- code-hive-web/src/App.tsx
+++ code-hive-web/src/App.tsx
@@ -1,21 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
 import VagasDev from "./pages/VagasDev";
 import VagasAdv from "./pages/VagasAdv";
-import CosmosBackground from "./components/CosmosBackground";
+import PlanetarySystem from "./components/PlanetarySystem";
+import Header from "./components/Header";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/vagas-dev" element={<VagasDev />} />
+        <Route path="/vagas-adv" element={<VagasAdv />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
-    <>
-      <CosmosBackground />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vagas-dev" element={<VagasDev />} />
-          <Route path="/vagas-adv" element={<VagasAdv />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <PlanetarySystem />
+      <div className="w-full min-h-screen flex flex-col overflow-hidden" style={{ position: 'relative', zIndex: 10 }}>
+        <Header />
+        <main className="flex-1 w-full relative">
+          <AnimatedRoutes />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
